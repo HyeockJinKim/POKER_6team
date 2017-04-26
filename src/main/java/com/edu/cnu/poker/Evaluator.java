26f2d0374c9bef7b74b39606aa808616b2c017c5
Sujin <djm03147@gmail.com>
2017-04-26 23:20:19 +0900

    스트레이트 ,백스트레이트

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -28,4 +28,65 @@ public class Evaluator {
         }
         return "NOTHING";
     }
+
+    public boolean straight(List<Card> cardList) {
+        Map<Suit, Integer> tempMap = new HashMap<Suit, Integer>();
+
+        for (Card card : cardList) {
+            if (tempMap.containsKey(card.getSuit())) {
+                Integer count = tempMap.get(card.getSuit());
+                count = new Integer(count.intValue() + 1);
+                tempMap.put(card.getSuit(), count);
+            } else {
+                tempMap.put(card.getSuit(), new Integer(1));
+            }
+        }
+        int c[] = new int[13];
+        int count = 0;
+        for (Suit key : tempMap.keySet()) {
+            c[tempMap.get(key)]++;
+        }
+
+
+        for (int i = 1; i <= 13; i++) {
+            if (c[i] == 1)
+                count++;
+            else
+                count = 0;
+        }
+        if (count == 5)
+            return true;
+        else
+            return false;
+    }
+    public boolean backstraight(List<Card> cardList){
+        Map<Suit, Integer> tempMap = new HashMap<Suit, Integer>();
+
+        for (Card card : cardList) {
+            if (tempMap.containsKey(card.getSuit())) {
+                Integer count = tempMap.get(card.getSuit());
+                count = new Integer(count.intValue() + 1);
+                tempMap.put(card.getSuit(), count);
+            } else {
+                tempMap.put(card.getSuit(), new Integer(1));
+            }
+        }
+        int c[] = new int[14];
+        int count = 0;
+        for (Suit key : tempMap.keySet()) {
+            c[tempMap.get(key)]++;
+        }
+        if(c[1]==1){
+            for (int i = 2; i <= 5; i++) {
+                if (c[i] != 1)
+                   return false;
+            }
+        }
+        else
+            return false;
+
+       return true;
+
+    }
+
 }
